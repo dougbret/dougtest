8d9d03d0e99ec3283639d265b60b3561af3e3baf
dougb <doug.brethower@lakedata.net>
2013-08-19 12:30:06 -0500

    Deleted content after move to u-sophia on nira/u-sophia github

--- u-sophia/settings/override/cronjob.ini.append.php
+++
@@ -1,9 +0,0 @@
-<?php /* #?ini charset="utf-8"?
-
-[linkCheckSettings]
-# Specify full path of all site URL
-# Example:
-# SiteURL[]=http://ezpublish.ez.no/admin
-# SiteURL[]=http://ezpublish.ez.no/news
-SiteURL[]=http://dev-u-sophia.com
-
